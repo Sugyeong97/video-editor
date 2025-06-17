52cd453845f8c7948c18732e7aba95ce691f88f7
LSG <sugyeong9611@gmail.com>
2025-06-17 22:43:35 +0900

    add video

--- src/components/molecules/YoutubePlayer.tsx
+++ src/components/molecules/YoutubePlayer.tsx
@@ -2,13 +2,26 @@ import YoutubeEmbed from "../atom/YoutubeEmbed";
 
 export default function YoutubePlayer() {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 my-5">
-      <h1 className="text-4xl">롱폼 영상</h1>
-      {/* 투어스 */}
-      <YoutubeEmbed videoId="tiifQw2nPZk?si=EvtwskZvcxkqnrNq" />
+    <div className="flex flex-col items-center justify-center gap-16 my-5">
+      <h1 className="text-6xl font-black">롱폼 영상</h1>
+      <div className="w-full max-w-[1600px] mx-auto flex flex-col gap-5">
+        {/* 투어스 */}
+        <p className="text-2xl">
+          📺 250519 투어스 위버스 라이브 [TWS 랜선 뒷풀이]
+        </p>
+        <YoutubeEmbed videoId="tiifQw2nPZk?si=EvtwskZvcxkqnrNq" />
+      </div>
 
       {/* 장지수 */}
-      <YoutubeEmbed videoId="JAHYpV8MEyU" />
+      <div className="w-full max-w-[1600px] mx-auto flex flex-col gap-5">
+        <p className="text-2xl">📺 210210 트위치 장지수 생일 라이브</p>
+        <YoutubeEmbed videoId="cqh8w_KofRw" />
+        <YoutubeEmbed videoId="G3Sa0L-v9F4" />
+        <YoutubeEmbed videoId="JAHYpV8MEyU" />
+        <YoutubeEmbed videoId="can1GeV_96I" />
+        <YoutubeEmbed videoId="eAKG46JfGsA" />
+        <YoutubeEmbed videoId="SJVUuBzPf2g" />
+      </div>
     </div>
   );
 }
