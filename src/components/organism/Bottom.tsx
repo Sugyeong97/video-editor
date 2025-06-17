fa85b2231dcae9f7295443fc6b54e291a1706895
LSG <sugyeong9611@gmail.com>
2025-06-17 23:18:48 +0900

    img-수정

--- src/components/organism/Bottom.tsx
+++ src/components/organism/Bottom.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Bottom() {
   return (
     <div className="flex flex-col items-center justify-center py-16 mb-10">
-      <img src="/bottom-img.png" alt="" />
+      <Image alt="" src="/bottom-img.png" width={1600} height={900} />
     </div>
   );
 }
--- src/components/organism/Myself.tsx
+++ src/components/organism/Myself.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Myself() {
   return (
     <div className="flex flex-col items-center justify-center py-16 mb-10">
-      <img src="/main-img-2.png" alt="" />
+      <Image alt="" src="/main-img-2.png" width={1600} height={900} />
     </div>
   );
 }
