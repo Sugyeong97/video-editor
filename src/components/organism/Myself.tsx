e7e00ec74800a9d6e33b11f7850064755463cecf
LSG <sugyeong9611@gmail.com>
2025-06-17 19:06:11 +0900

    main-img

--- src/components/organism/Myself.tsx
+++ src/components/organism/Myself.tsx
@@ -1,7 +1,7 @@
 export default function Myself() {
   return (
     <div className="flex flex-col items-center justify-center py-5">
-      <img src="/main-img.png" alt="" />
+      <img src="/main-img-2.png" alt="" />
     </div>
   );
 }
